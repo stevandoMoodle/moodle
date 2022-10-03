5287749350ff7bc8636c41f2ddfaafd17af5d65c
Stevani Andolo <stevani.andolo@lumoshive.com>
2022-10-03 09:51:50 +0800

    MDL-75262 tiny_accesiblity: Deleted the unused lang strings

--- lib/editor/tiny/plugins/accessibilitychecker/lang/en/tiny_accessibilitychecker.php
+++ lib/editor/tiny/plugins/accessibilitychecker/lang/en/tiny_accessibilitychecker.php
@@ -22,16 +22,12 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['check_title'] = 'Accessbility check confirmation';
-$string['check_desc'] = 'Do you want check the accessibility issues for everyting?<br><i><b>Cancel</b></i> will check the accessibility issues for the selected element or where the cursor is located.';
-$string['confirm_yes'] = 'Yes';
 $string['emptytext'] = 'Empty text';
 $string['entiredocument'] = 'Entire document';
 $string['imagesmissingalt'] = '<i><b>Images</b></i> require alternative text. To fix this warning, add an alt attribute to your img tags. An empty alt attribute may be used, but only when the image is purely decorative and carries no information.';
 $string['needsmorecontrast'] = 'The colours of the foreground and background text do not have enough contrast. To fix this warning, change either foreground or background colour of the text so that it is easier to read.';
 $string['needsmoreheadings'] = 'There is a lot of text with no <i><b>headings</b></i>. <i><b>Headings</b></i> will allow screen reader users to navigate through the page easily and will make the page more usable for everyone.';
 $string['nowarnings'] = 'Congratulations, no accessibility issues found!';
-$string['nowarningonselected'] = 'Congratulations, the selected element has no accessibility issues found!';
 $string['pluginname'] = 'Accessibility checker';
 $string['report'] = 'Accessibility report:';
 $string['tablesmissingcaption'] = '<i><b>Tables</b></i> should have captions. While it is not necessary for each table to have a caption, a caption is generally very helpful.';
