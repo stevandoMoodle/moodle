f6db6d74b2e0e0208a15d22d3846db4429c11e21
meirzamoodle <meirza.arson@moodle.com>
2025-04-08 11:10:35 +0700

    MDL-84832 core: Feedback for the screen reader when maxlength is reached
    The JS will check the page for input and text area elements
    with the maxlength attribute, then the script will continue
    sending feedback to the screen reader if the character limit
    has been reached.

--- lib/db/hooks.php
+++ lib/db/hooks.php
@@ -139,4 +139,8 @@ $callbacks = [
         'callback' => [\core_courseformat\hook_listener::class, 'before_course_viewed'],
         'priority' => 999,
     ],
+    [
+        'hook' => core\hook\output\before_footer_html_generation::class,
+        'callback' => [\core\accessibility\maxlength_announcer::class, 'before_footer_html_generation'],
+    ],
 ];
