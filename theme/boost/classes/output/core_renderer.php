b7199aab39beb37f3fbd7d2823e9f5b53c2d13aa
Stevani Andolo <stevani.andolo@moodle.com>
2025-05-28 10:25:48 +0800

    suggestion

--- theme/boost/classes/output/core_renderer.php
+++ theme/boost/classes/output/core_renderer.php
@@ -127,7 +127,8 @@ class core_renderer extends \core_renderer {
                         )
                     );
 
-                    if ($USER->id != $user->id && \core_message\api::can_create_contact($USER->id, $user->id)) {
+                    if ($USER->id != $user->id) {
+                        $cancreatecontact = \core_message\api::can_create_contact($USER->id, $user->id);
                         $iscontact = \core_message\api::is_contact($USER->id, $user->id);
                         $isrequested = \core_message\api::get_contact_requests_between_users($USER->id, $user->id);
                         $contacturlaction = '';
@@ -140,6 +141,9 @@ class core_renderer extends \core_renderer {
                         // If the user is not a contact.
                         if (!$iscontact) {
                             if ($isrequested) {
+                                // Set it to true if a request has been sent.
+                                $cancreatecontact = true;
+
                                 // We just need the first request.
                                 $requests = array_shift($isrequested);
                                 if ($requests->userid == $USER->id) {
@@ -165,19 +169,22 @@ class core_renderer extends \core_renderer {
                             $contacturlaction = 'removecontact';
                             $contactimage = 't/removecontact';
                         }
-                        $userbuttons['togglecontact'] = array(
-                                'buttontype' => 'togglecontact',
-                                'title' => get_string($contacttitle, 'message'),
-                                'url' => new moodle_url('/message/index.php', array(
-                                        'user1' => $USER->id,
-                                        'user2' => $user->id,
-                                        $contacturlaction => $user->id,
-                                        'sesskey' => sesskey())
-                                ),
-                                'image' => $contactimage,
-                                'linkattributes' => $linkattributes,
-                                'page' => $this->page
-                            );
+
+                        if ($cancreatecontact) {
+                            $userbuttons['togglecontact'] = array(
+                                    'buttontype' => 'togglecontact',
+                                    'title' => get_string($contacttitle, 'message'),
+                                    'url' => new moodle_url('/message/index.php', array(
+                                            'user1' => $USER->id,
+                                            'user2' => $user->id,
+                                            $contacturlaction => $user->id,
+                                            'sesskey' => sesskey())
+                                    ),
+                                    'image' => $contactimage,
+                                    'linkattributes' => $linkattributes,
+                                    'page' => $this->page
+                                );
+                        }
                     }
 
                     $this->page->requires->string_for_js('changesmadereallygoaway', 'moodle');
