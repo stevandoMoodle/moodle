373dc6f03c1272b67e64ac38378afd320c619861
Adrian Greeve <abgreeve@gmail.com>
2025-11-27 15:42:52 +0800

    Update to mod book to show react working with dynamic forms
    This is proving that we don't need the shim

--- public/mod/book/view.php
+++ public/mod/book/view.php
@@ -121,7 +121,8 @@ if (!$chapterid) {
     echo $OUTPUT->header();
 
     echo $OUTPUT->render_from_template('mod_book/book-test', []);
-    $PAGE->requires->js_react_init('./react/build/travel.js', '#book-react-node');
+    $PAGE->requires->js_type_module('./react/build/travel.js');
+    // $PAGE->requires->js_react_init('./react/build/travel.js', '#book-react-node');
 
     $renderer = $PAGE->get_renderer('mod_book');
     $actionmenu = new \mod_book\output\main_action_menu($cm->id, $chapters, $chapter, $book);
