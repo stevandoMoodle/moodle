306b9ba66c16c578af3b0f3650f86e1b5ccc08e6
Adrian Greeve <adrian@moodle.com>
2025-12-03 16:05:43 +0800

    Updated autoinit for ESM compatibility

--- public/lib/react/build/jsx-dev-runtime.js
+++ public/lib/react/build/jsx-dev-runtime.js
@@ -1,12 +1,12 @@
-var a=Object.create;var m=Object.defineProperty;var x=Object.getOwnPropertyDescriptor;var d=Object.getOwnPropertyNames;var j=Object.getPrototypeOf,v=Object.prototype.hasOwnProperty;var c=(r,e)=>()=>(e||r((e={exports:{}}).exports,e),e.exports);var f=(r,e,t,i)=>{if(e&&typeof e=="object"||typeof e=="function")for(let s of d(e))!v.call(r,s)&&s!==t&&m(r,s,{get:()=>e[s],enumerable:!(i=x(e,s))||i.enumerable});return r};var l=(r,e,t)=>(t=r!=null?a(j(r)):{},f(e||!r||!r.__esModule?m(t,"default",{value:r,enumerable:!0}):t,r));var n=c(o=>{"use strict";var g=Symbol.for("react.fragment");o.Fragment=g;o.jsxDEV=void 0});var u=c((N,p)=>{"use strict";p.exports=n()});var E=l(u());var export_Fragment=E.Fragment;var export_jsxDEV=E.jsxDEV;export{export_Fragment as Fragment,export_jsxDEV as jsxDEV};
+var x=Object.create;var m=Object.defineProperty;var d=Object.getOwnPropertyDescriptor;var j=Object.getOwnPropertyNames;var v=Object.getPrototypeOf,f=Object.prototype.hasOwnProperty;var a=(r,e)=>()=>(e||r((e={exports:{}}).exports,e),e.exports);var l=(r,e,t,i)=>{if(e&&typeof e=="object"||typeof e=="function")for(let s of j(e))!f.call(r,s)&&s!==t&&m(r,s,{get:()=>e[s],enumerable:!(i=d(e,s))||i.enumerable});return r};var E=(r,e,t)=>(t=r!=null?x(v(r)):{},l(e||!r||!r.__esModule?m(t,"default",{value:r,enumerable:!0}):t,r));var c=a(o=>{"use strict";var g=Symbol.for("react.fragment");o.Fragment=g;o.jsxDEV=void 0});var p=a((q,n)=>{"use strict";n.exports=c()});var u=E(p());var export_Fragment=u.Fragment;var export_jsxDEV=u.jsxDEV;export{export_Fragment as Fragment,export_jsxDEV as jsxDEV};
 /*! Bundled license information:
 
-react/cjs/react-jsx-dev-runtime.production.js:
+react/cjs/react-jsx-dev-runtime.production.min.js:
   (**
    * @license React
-   * react-jsx-dev-runtime.production.js
+   * react-jsx-dev-runtime.production.min.js
    *
-   * Copyright (c) Meta Platforms, Inc. and affiliates.
+   * Copyright (c) Facebook, Inc. and its affiliates.
    *
    * This source code is licensed under the MIT license found in the
    * LICENSE file in the root directory of this source tree.
