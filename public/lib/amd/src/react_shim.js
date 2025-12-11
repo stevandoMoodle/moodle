acd502b15f841864a630e277262481ed0b649e5e
Adrian Greeve <abgreeve@gmail.com>
2025-12-11 09:36:36 +0800

    AE-100: Removed unneeded files out of the public/lib/react folder

--- public/lib/amd/src/react_shim.js
+++
@@ -1,8 +0,0 @@
-export const init = (selector, options = {}) => {
-    if (window.ReactApp && typeof window.ReactApp.init === 'function') {
-        window.ReactApp.init(selector, options);
-        return;
-    }
-
-    window.console.warn('window.ReactApp.init not found');
-};
