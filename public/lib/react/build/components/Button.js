cf7acdd252eba44dbb1554a05829c08b25da5b0f
Adrian Greeve <abgreeve@gmail.com>
2025-12-11 09:36:36 +0800

    AE-100: Removed unneeded files out of the public/lib/react folder

--- public/lib/react/build/components/Button.js
+++
@@ -1,2 +0,0 @@
-import b,{useState as m}from"react";function f(s){let{label:t,type:a="button",disabled:u=!1,loading:e=!1,onClick:n,className:i="",...c}=s,o=u||e,r=c,[B,d]=m(0);return b.createElement("button",{type:a,disabled:o,onClick:l=>{if(o){l.preventDefault();return}d(p=>p+1),n&&n(l)},className:i,...r},e?t?`${t}\u2026`:"Loading\u2026":t)}export{f as default};
-//# sourceMappingURL=Button.js.map
