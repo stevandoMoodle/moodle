a5578749e1134a911a62f9f04d0ad89c923cfd72
meirzamoodle <meirza.arson@moodle.com>
2025-12-18 10:34:01 +0800

    AE-95: Test multiple React helper elements on a single page
    This is a local test plugin

---
+++ public/local/multiplereact/index.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Multiple React test page.
+ *
+ * @package    local_multiplereact
+ * @copyright  Meirza <meirza.arson@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../config.php');
+
+require_login();
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new \core\url('/local/multiplereact/index.php'));
+$PAGE->set_title('Multiple React test');
+$PAGE->set_heading('Multiple React test');
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->render_from_template(
+    'local_multiplereact/local_multiplereact_test',
+    [
+        'who' => fullname($USER),
+    ]
+);
+
+echo $OUTPUT->footer();
---
+++ public/local/multiplereact/react/src/local_multiplereact_test.tsx
@@ -0,0 +1,12 @@
+import { React } from "@moodle/core/react";
+
+type Props = { who: string };
+
+export default function LocalMultipleReactTest({ who }: Props) {
+    return (
+        <div>
+            <strong>Hello from local_multiplereact 👋</strong>
+            <div>User: {who}</div>
+        </div>
+    );
+}
