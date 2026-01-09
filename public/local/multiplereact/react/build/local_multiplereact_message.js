d6d7c1bfc3fcd076b22f86602adceb9c14ce5d60
Adrian Greeve <adrian@moodle.com>
2026-01-09 14:47:04 +0800

    AE-122: Moving platform build code into its own directory and files.

--- public/local/multiplereact/react/build/local_multiplereact_message.js
+++ public/local/multiplereact/react/build/local_multiplereact_message.js
@@ -1,2 +1,1 @@
 import{React as s}from"../../../../lib/react/build/react.js";function e({message:r}){return s.createElement("div",null,s.createElement("strong",null,"Message"),s.createElement("div",null,r))}export{e as default};
-//# sourceMappingURL=local_multiplereact_message.js.map
--- public/mod/book/react/build/mustache_test.js
+++ public/mod/book/react/build/mustache_test.js
@@ -1,2 +1,1 @@
 import{React as t}from"../../../../lib/react/build/react.js";import{withProfiler as n}from"../../../../lib/react/build/profiler.js";function u({title:s="MustacheTest Component",message:o="Hello!"}){let[e,r]=t.useState(0);return t.createElement("div",{className:"alert alert-success"},t.createElement("h4",null,s),t.createElement("p",null,o),t.createElement("button",{className:"btn btn-primary",onClick:()=>r(e+1)},"Count: ",e))}var c=n(u,"MustacheTest");export{c as default};
-//# sourceMappingURL=mustache_test.js.map
